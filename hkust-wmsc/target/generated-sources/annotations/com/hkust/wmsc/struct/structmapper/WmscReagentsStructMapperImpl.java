8da629cfe4217fb579cc428c0933d12c80151325
UglyKim <wenjieok@hotmail.com>
2025-04-15 11:06:15 +0800

    commit

--- hkust-wmsc/target/generated-sources/annotations/com/hkust/wmsc/struct/structmapper/WmscReagentsStructMapperImpl.java
+++ hkust-wmsc/target/generated-sources/annotations/com/hkust/wmsc/struct/structmapper/WmscReagentsStructMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-14T08:06:03+0800",
+    date = "2025-04-15T11:06:03+0800",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 1.8.0_391 (Oracle Corporation)"
 )
 @Component
